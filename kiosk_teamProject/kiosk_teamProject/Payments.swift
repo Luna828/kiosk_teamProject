d091cde76115e25b8cda20b1b495cf0b7c6aa1aa
KangJiHun1028 <136081642+KangJiHun1028@users.noreply.github.com>
2023-07-28 11:36:29 +0900

    오류

---
+++ kiosk_teamProject/kiosk_teamProject/Payments.swift
@@ -0,0 +1,44 @@
+import Foundation
+
+struct Frozens {
+    
+    let menuList: [Goods] = [
+        Goods(name: "Floats", price: 5.9, content: "부드러운 바닐라 커스터드와 탄산 플로트"),
+        Goods(name: "SmokCup & Cones SingleeShack", price: 4.9, content: "부드럽고 쫀득한 바닐라, 초코 커스터드"),
+        Goods(name: "Cup & Cones Double", price: 5.9, content: "부드럽고 쫀득한 바닐라, 초코 커스터드"),
+        Goods(name: "Featured Shakes", price: 6.5, content: "쿠키 & 크림 쉐이크, 바닐라/초콜렛/딸기"),
+        Goods(name: "Shakes", price: 5.9, content: "솔티드 카라멜, 피넛버터, 커피")
+    ]
+    
+    func showFrozenMenu(){
+        
+        print("[ Frozen Custard MENU ]")
+        for (index, menu) in menuList.enumerated() {
+            let paddedName = menu.name.padding(toLength: 15, withPad: " ", startingAt: 0)
+            print("\(index + 1). \(paddedName)| W\(menu.price) | \(menu.content)")
+        }
+        print("0. 뒤로가기")
+        print("---------------------------------------------------")
+        var Running = true
+        while Running {
+            guard let numStr = readLine(),
+                  let num = Int(numStr) else {
+                print("번호를 잘못 입력하셨습니다")
+                continue
+            }
+            
+            switch num {
+            case 1...menuList.count:
+                let selectedItem = menuList[num - 1]
+                addItem(selectedItem)
+                Running = false
+            case 0 :
+                print("뒤로가기")
+                Running = false
+            default:
+                print("다시 선택해주세요")
+            }
+        }
+    }
+    
+}
--- kiosk_teamProject/kiosk_teamProject/main.swift
+++ kiosk_teamProject/kiosk_teamProject/main.swift
@@ -13,6 +13,32 @@ class Kiosk {
     func shakeShack() {
        
         print("[ SHAKESHACK MENU ]")
+var accountList = [Payments]()
+var account = newAccount(name: "Daisy", balance: Double.random(in: 5000...1000000))
+
+
+func newAccount(name: String, balance: Double) -> Payments {
+    let account = Payments(accountName: name, currentBalance: balance)
+    accountList.append(account)
+    return account
+}
+
+let menuList: [Menu] = [
+    Menu(name: "Burgers", content: "앵거스 비프 통살을 다져만든 버거"),
+    Menu(name: "Frozen Custard", content: "매장에서 신선하게 만드는 아이스크림"),
+    Menu(name: "Drinks", content: "매장에서 직접 만드는 음료"),
+    Menu(name: "Beer", content: "뉴욕 브루클린 브루어리에서 양조한 맥주"),
+    Menu(name: "Cart", content: ""),
+    Menu(name: "Exit", content: "프로그램 종료")
+]
+var cartList = [Goods]()
+func showTitle() {
+    print("""
+    "SHAKESHACK BURGER 에 오신걸 환영합니다."
+    아래 메뉴판을 보시고 메뉴를 골라 입력해주세요.
+
+    [ SHAKESHACK MENU ]
+    """)
         for (index, menu) in menuList.enumerated() {
             // toLength: 새로운 문자열의 길이를 15로 설정
             // withPad: "" 추가할 공백 문자를 지정 '|' 사이의 간격을 맞추도록 지정
@@ -51,6 +77,56 @@ class Kiosk {
             }
         }
     }
+}
+func addItem(_ item: Goods) { // kiosk 인자 추가
+    print("\(item.name)을 장바구니에 추가하시겠습니까?")
+    print("1. 확인           2.취소")
+    print("---------------------------------------------------")
+    var isRunning = true
+    while isRunning {
+        guard let numStr = readLine(),
+              let num = Int(numStr) else {
+            print("번호를 잘못 입력하셨습니다")
+            continue
+        }
+        
+        switch num {
+        case 1 :
+            cartList.append(item)
+            print("---------------------------------------------------")
+            print("\(item.name)을 장바구니에 추가하였습니다.")
+            print("메뉴판으로 이동합니다.")
+            print("---------------------------------------------------")
+            isRunning = false
+        case 2 :
+            isRunning = false
+        default:
+            print("번호를 눌러주세요")
+            continue
+        }
+    }
+}
+
+func showCart() {
+    var totalPrice : Double = 0
+    if cartList.isEmpty {
+        print("카트의 장바구니가 비어 있습니다.")
+    } else {
+        print("[ 장바구니 ]")
+        for (index, item) in cartList.enumerated() {
+            let paddedName = item.name.padding(toLength: 15, withPad: " ", startingAt: 0)
+            print("\(index + 1). \(paddedName)| W\(item.price) | \(item.content)")
+            totalPrice += item.price
+        }
+        print("""
+              ---------------------------------------------------
+                            총결제금액: \(totalPrice)
+              ---------------------------------------------------
+              """)
+        Payments.withdraw(totalPrice)
+        
+    }
+    
 }
 
 Kiosk().shakeShack()
